0d959dfceb99902ac56b0c99f61a97526fcde07c
aikene <ikene.aj@gmail.com>
2023-03-16 23:39:34 -0400

    Update buildspec

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["3.84.7.210"]/' /home/ubuntu/django-aws_cicd/main/settings.py
+sed -i 's/\[]/\["3.84.7.210"]/' /home/ubuntu/django-aws_cicd/awscicd/settings.py
 sudo systemctl restart gunicorn
 sudo systemctl restart nginx
