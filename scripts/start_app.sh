8c8ad22023dc55ddfd89eac8ee571cd827454033
aikene <ikene.aj@gmail.com>
2023-03-17 00:05:49 -0400

    Update deployment

--- scripts/start_app.sh
+++ scripts/start_app.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/bash 
 
-sed -i 's/\[]/\["3.84.7.210"]/' /home/ubuntu/django-aws_cicd/awscicd/settings.py
+sed -i 's/\[]/\["18.210.11.217"]/' /home/ubuntu/django-aws_cicd/awscicd/settings.py
 sudo systemctl restart gunicorn
 sudo systemctl restart nginx
