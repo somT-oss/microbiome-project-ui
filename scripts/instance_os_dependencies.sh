1ab138db93ce54274bb4a4d81d2c57ec853708be
aikene <ikene.aj@gmail.com>
2023-03-19 13:58:37 -0400

    reverting

--- scripts/instance_os_dependencies.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-sudo apt install -y python3-pip
-sudo apt install -y nginx
-sudo apt install -y virtualenv
-sudo apt install libmysqlclient-dev
--- scripts/start_app.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/bash 
-
-sed -i 's/\[]/\["18.210.11.217"]/' /home/ubuntu/django-aws_cicd/awscicd/settings.py
-sudo systemctl restart gunicorn
-sudo systemctl restart nginx
